cd8a6e71df4460306f7127171a33baa2148560cc
lkabb <lkabb1996@foxmail.com>
2019-12-18 16:36:46 +0800

    Hello

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+		System.out.println("1");
+		System.out.println("Hello!");
+		System.out.println("1");
+		System.out.println("Hello!");
+	}
+}
